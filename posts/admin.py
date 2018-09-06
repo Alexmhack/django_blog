c5f6cc0de46d43715c07d63682188ef0acdf2da6
alexHooper <alexmohack@gmail.com>
2018-09-06 20:13:52 +0530

    create model admin for post model

--- posts/admin.py
+++ posts/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+	list_display = ('title', 'timestamp', 'updated')
+	list_display_links = ('title', 'timestamp')
+
+	class Meta:
+		model = Post
+
+
+admin.site.register(Post, PostModelAdmin)
