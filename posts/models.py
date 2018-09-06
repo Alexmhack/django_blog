fcebffa7382866246e66406693b9117034ed65e2
alexHooper <alexmohack@gmail.com>
2018-09-06 19:33:26 +0530

    create model for posts app

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=200)
+	content = models.TextField()
+	timestamp = models.DateTimeField(auto_now_add=True)
+	upadted = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.title
