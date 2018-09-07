da668c9eb62add10c1fa7cea8622f164ae698116
alexHooper <alexmohack@gmail.com>
2018-09-07 19:57:07 +0530

    change the url names

--- posts/urls.py
+++ posts/urls.py
@@ -12,8 +12,8 @@ app_name = 'posts'
 
 urlpatterns = [
 	path('create/', post_create, name='posts-home'),
-	path('detail/', post_detail, name='posts-home'),
-	path('update/', post_update, name='posts-home'),
-	path('list/', post_list, name='posts-home'),
-	path('delete/', post_delete, name='posts-home'),
+	path('detail/', post_detail, name='posts-detail'),
+	path('update/', post_update, name='posts-update'),
+	path('list/', post_list, name='posts-list'),
+	path('delete/', post_delete, name='posts-delete'),
 ]
