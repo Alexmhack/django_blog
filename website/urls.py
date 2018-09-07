43b348956c9670712749b7ac5e4d39dc76baf577
alexHooper <alexmohack@gmail.com>
2018-09-07 19:20:01 +0530

    create view and url for posts home page

--- website/urls.py
+++ website/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from posts.views import posts_home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('posts/', posts_home, name='posts-home'),
 ]
