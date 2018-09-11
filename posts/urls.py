312f0ee9c333145924ebaeecde6afecf6dfc31cc
alexHooper <alexmohack@gmail.com>
2018-09-11 22:54:01 +0530

    edit detail view url

--- posts/urls.py
+++ posts/urls.py
@@ -12,7 +12,7 @@ app_name = 'posts'
 
 urlpatterns = [
 	path('create/', post_create, name='posts-home'),
-	path('detail/<int:id>', post_detail, name='posts-detail'),
+	path('<int:id>/', post_detail, name='posts-detail'),
 	path('update/', post_update, name='posts-update'),
 	path('list/', post_list, name='posts-list'),
 	path('delete/', post_delete, name='posts-delete'),
