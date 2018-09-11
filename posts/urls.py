02aeea8d637ca44a120d82b620327f197ff963a3
alexHooper <alexmohack@gmail.com>
2018-09-11 23:21:46 +0530

    remove the extra posts- from url names

--- posts/urls.py
+++ posts/urls.py
@@ -11,9 +11,9 @@ from posts.views import (
 app_name = 'posts'
 
 urlpatterns = [
-	path('create/', post_create, name='posts-home'),
-	path('<int:id>/', post_detail, name='posts-detail'),
-	path('update/', post_update, name='posts-update'),
-	path('list/', post_list, name='posts-list'),
-	path('delete/', post_delete, name='posts-delete'),
+	path('create/', post_create, name='home'),
+	path('<int:id>/', post_detail, name='detail'),
+	path('update/', post_update, name='update'),
+	path('list/', post_list, name='list'),
+	path('delete/', post_delete, name='delete'),
 ]
