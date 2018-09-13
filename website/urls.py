2622a235fb8ae70e601b0c98db2f79ab6049c97f
alexHooper <alexmohack@gmail.com>
2018-09-13 18:21:57 +0530

    change urls for static files

--- website/urls.py
+++ website/urls.py
@@ -21,4 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls', namespace='posts')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
