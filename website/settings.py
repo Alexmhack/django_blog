7b157ece487663d13fda4e9f47165b7a670636a2
alexHooper <alexmohack@gmail.com>
2018-09-13 18:30:22 +0530

    correct the dirname error

--- website/settings.py
+++ website/settings.py
@@ -124,4 +124,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR, 'static_cdn'))
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn')
